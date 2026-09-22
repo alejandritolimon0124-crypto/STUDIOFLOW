3d6f2ed2fb6c4e823cf230f97ffa2aafe20befe3
Dr Alex Mercado <alejandritolimon0124@gmail.com>
2026-09-22 17:08:25 -0600

    security: restrict push trigger functions

---
+++ supabase/migrations/202609220004_lock_push_trigger_functions.sql
@@ -0,0 +1,5 @@
+revoke all on function public.studio_flow_queue_appointment_push()
+from public, anon, authenticated;
+
+revoke all on function public.studio_flow_queue_client_notice_push()
+from public, anon, authenticated;
